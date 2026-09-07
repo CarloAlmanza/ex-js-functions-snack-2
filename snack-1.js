c4762443b9ce3ebf78888d830c81a134a5203fce
CarloAlmanza <almanzacarlo6@gmail.com>
2026-09-07 11:13:02 +0200

    snack-1

---
+++ snack-1.js
@@ -0,0 +1,12 @@
+// 1. Funzione dichiarativa
+function somma(a, b) {
+    return a + b;
+}
+
+// 2. Funzione anonima assegnata a una variabile
+const somma = function (a, b) {
+    return a + b;
+};
+
+// 3. Arrow function
+const somma = (a, b) => a + b;
